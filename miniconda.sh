e3c9e053e43eceb8de0635f4500687630beb0dbd
Le-Vu Tran <tlvu2697@outlook.com>
2018-08-29 03:13:09 +0000

    Edit miniconda.sh

--- miniconda.sh
+++ miniconda.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Download lastest Miniconda
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
+
+# Install in Batch mode
+sh Miniconda3-latest-Linux-x86_64.sh -b
+
+# Export PATH
+export PATH=/root/miniconda3/bin:$PATH
